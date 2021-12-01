5c9dcdb557ad06c165f4d83967f772bdfe529d6c
nathen1 <nathen.e.gay@gmail.com>
2021-12-01 18:31:04 -0500

    added music files

--- src/newApproach.py
+++ src/newApproach.py
@@ -5,8 +5,8 @@ import math
 import time
 
 # Starting points for ROI
-cap_region_x_begin=0 
-cap_region_y_end=0.5
+cap_region_x_begin = 0
+cap_region_y_end = 0.5
 # cap_region_y_end=1
 
 # Parameter default values
@@ -14,12 +14,12 @@ threshold = 128  # Binary threshold
 blurValue = 41  # GaussianBlur parameter
 bgSubThreshold = 50
 learningRate = 0
-devMode = 0 # 0 = ON
+devMode = 0  # 0 = ON
 
 # Initializing Variables
-startCounting = False  
+startCounting = False
 bgModel = cv2.createBackgroundSubtractorMOG2(0, bgSubThreshold)
-prev_frame_time = 0 # For FPS
+prev_frame_time = 0  # For FPS
 new_frame_time = 0
 
 # add
@@ -30,18 +30,16 @@ hasBackground = False
 key_pressed = []
 
 
-# Initialize Piano image that will be used for reference
-piano_img = cv2.imread('../image_files/piano.jpg')
-
 # Methods ###############################################################################
 def removeBG(frame):
-    fgmask = bgModel.apply(frame,learningRate=learningRate)
+    fgmask = bgModel.apply(frame, learningRate=learningRate)
     kernel = np.ones((3, 3), np.uint8)
     fgmask = cv2.erode(fgmask, kernel, iterations=1)
     res = cv2.bitwise_and(frame, frame, mask=fgmask)
     return res
 
-def calculateFingers(res,drawing):
+
+def calculateFingers(res, drawing):
     # add
     global finger_tip_x, finger_tip_y
     finger_tip_x = []
@@ -53,7 +51,7 @@ def calculateFingers(res,drawing):
         defects = cv2.convexityDefects(res, hull)
         if type(defects) != type(None):
             cnt = 0
-            for i in range(defects.shape[0]): # Calculating the Angle
+            for i in range(defects.shape[0]):  # Calculating the Angle
                 s, e, f, d = defects[i][0]
                 start = tuple(res[s][0])
                 end = tuple(res[e][0])
@@ -61,91 +59,70 @@ def calculateFingers(res,drawing):
                 a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
                 b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
                 c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
-                angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c)) # COS
+                angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))  # COS
                 if angle <= math.pi / 2:  # if angle <90, it's a finger
-        
+
                     # add
                     cv2.circle(drawing, start, 8, [255, 255, 255], -1)
                     cv2.circle(drawing, end, 8, [200, 200, 200], -1)
-                    
+
                     if cnt:
-                        finger_tip_x.pop() 
+                        finger_tip_x.pop()
                         finger_tip_y.pop()
-                    
-                    finger_tip_x.append(start[0]) 
+
+                    finger_tip_x.append(start[0])
                     finger_tip_y.append(start[1])
                     finger_tip_x.append(end[0])
                     finger_tip_y.append(end[1])
-                    # finger_tip_x.append(start[0]) 
+                    # finger_tip_x.append(start[0])
                     # finger_tip_y.append(start[1])
                     # if i == defects.shape[0]:
                     #     finger_tip_x.append(end[0])
                     #     finger_tip_y.append(end[1])
-                    
+
                     # original
                     cnt += 1
                     cv2.circle(drawing, far, 8, [211, 84, 0], -1)
 
-                    
-
-
             return True, cnt
     return False, 0
+
+
 # End of Methods ########################################################################
-
-# Canny Edge detector Method, counting keys on piano
-def canny_edge_detector(image):
-    # reduce noise using 3 x 3  gaussian blur
-    noise_reduction = cv2.GaussianBlur(image, (3, 3), 0)
-
-    # convert to Grayscale
-    piano_img_grayscale = cv2.cvtColor(noise_reduction, cv2.COLOR_BGR2GRAY)
-
-    # Canny Edge Detection
-    canny_image = cv2.Canny(piano_img_grayscale, 50, 200)
-
-    contours, hierarchy = cv2.findContours(canny_image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    num_objects = len(contours)
-    print("Number of Objects Detected: " + str(num_objects))
-    return num_objects
-
-
 
 # Main
 video = cv2.VideoCapture(0)
-video.set(10,200)
-num_piano_keys = canny_edge_detector(piano_img)
+video.set(10, 200)
 
 while video.isOpened():
     ret, frame = video.read()
     frame = cv2.bilateralFilter(frame, 5, 50, 100)  # smoothing filter
     frame = cv2.flip(frame, 1)  # Flip the webcam horizontally
-    
+
     # Calculating FPS
     new_frame_time = time.time()
-    fps = 1/(new_frame_time-prev_frame_time)
+    fps = 1 / (new_frame_time - prev_frame_time)
     prev_frame_time = new_frame_time
-    fps = int(fps) 
+    fps = int(fps)
     fps = str(fps)
-    
+
     # Show FPS
     font = cv2.FONT_HERSHEY_SIMPLEX
     cv2.putText(frame, fps, (7, 70), font, 3, (100, 255, 0), 3, cv2.LINE_AA)
 
     # Need to invert the rectangle to take the lower half portion of the screen
     cv2.rectangle(frame, (int(cap_region_x_begin * frame.shape[1]), 0),
-                 (frame.shape[1], int(cap_region_y_end * frame.shape[0])), (255, 0, 0), 2)
+                  (frame.shape[1], int(cap_region_y_end * frame.shape[0])), (255, 0, 0), 2)
     if devMode == 0:
         cv2.imshow('original', frame)
 
     #  Applying hand mask
     img = removeBG(frame)
-    img = img[int(cap_region_y_end * frame.shape[0]):int(frame.shape[0]),
-                    int(cap_region_x_begin * frame.shape[1]):frame.shape[1]]  # clip the ROI
+    img = img[0:int(cap_region_y_end * frame.shape[0]),
+          int(cap_region_x_begin * frame.shape[1]):frame.shape[1]]  # clip the ROI
     if devMode == 0:
         cv2.imshow('mask', img)
 
-    
     # Convert Image for Contours
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (blurValue, blurValue), 0)
@@ -177,7 +154,7 @@ while video.isOpened():
         cv2.drawContours(drawing, [hull], 0, (0, 0, 255), 3)
 
         # Counting fingers is off by one
-        isFinishCal,cnt = calculateFingers(res,drawing)
+        isFinishCal, cnt = calculateFingers(res, drawing)
 
         # add
         for i in range(len(finger_tip_x)):
@@ -189,28 +166,25 @@ while video.isOpened():
         flow = optical_flow_method(background_img, gray, None, *params)
         for i in range(len(finger_tip_x)):
             flowat = flow[finger_tip_y[i], finger_tip_x[i]]
-            cv2.line(frame, (finger_tip_x[i], finger_tip_y[i]), (finger_tip_x[i] + round(flowat[1]), finger_tip_y[i] + round(flowat[0])), (255, 0, 0))
-            angle = math.atan2(finger_tip_y[i] - (finger_tip_y[i] + round(flowat[0])), finger_tip_x[i] - (finger_tip_x[i] + round(flowat[1]))) * 180 / math.pi
+            cv2.line(frame, (finger_tip_x[i], finger_tip_y[i]),
+                     (finger_tip_x[i] + round(flowat[1]), finger_tip_y[i] + round(flowat[0])), (255, 0, 0))
+            angle = math.atan2(finger_tip_y[i] - (finger_tip_y[i] + round(flowat[0])),
+                               finger_tip_x[i] - (finger_tip_x[i] + round(flowat[1]))) * 180 / math.pi
             print("angle: ", angle)
 
             if angle < -80 and angle > -100:
                 key_pressed.append((finger_tip_x[i], finger_tip_y[i]))
-                
-        
+
         cv2.imshow('optical flow', frame)
         print("key pressed", key_pressed)
 
         if startCounting is True:
             if isFinishCal is True:
-                print (cnt)                                       
+                print(cnt)
     if devMode == 0:
-        cv2.imshow('Contours', drawing) 
-    
+        cv2.imshow('Contours', drawing)
 
-
-
-
-    # Waiting to kill program
+        # Waiting to kill program
     k = cv2.waitKey(10)
     if k == 27:  # ESC key
         video.release()
@@ -218,4 +192,4 @@ while video.isOpened():
         break
     elif k == ord('c'):
         startCounting = True
-        print ('Starting the counter')
+        print('Starting the counter')
