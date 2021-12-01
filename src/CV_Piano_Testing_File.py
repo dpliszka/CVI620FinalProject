f70af805139dd4b6e0e1d7b242d65fc9fbc598d3
Nathen Gay <43282271+nathen1@users.noreply.github.com>
2021-12-01 09:36:31 -0500

    added working file for piano key extraction

---
+++ src/CV_Piano_Testing_File.py
@@ -0,0 +1,2 @@
+# The purpose of this file is to use different CV techniques to read an image of a piano, and extract the keys from
+# said image. The best function will be incorporated into the main project.
